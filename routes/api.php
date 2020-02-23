a80bae1cd5648e2328ff2736e64e56168ac27b25
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2020-02-23 13:43:49 -0500

    groupRoute

--- routes/api.php
+++ routes/api.php
@@ -9,13 +9,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group([
-        'middleware' => ['api', 'cors'],
-        'prefix' => 'api',
-    ], function ($router) {
-         //Add you routes here, for example:
-         Route::post('login', 'APILoginController@login');
-    });
+Route::post('login', 'APILoginController@login');
 
 //Route::post('logout', 'APILoginController@logout');
 
