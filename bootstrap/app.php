3039568300fc4ad32cf8a70d88ad863fffb5972c
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2020-02-23 13:48:45 -0500

    groupRoute

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,9 +17,6 @@ $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
 );
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
 
 /*
 |--------------------------------------------------------------------------
