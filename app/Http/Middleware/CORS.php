a3393154d34e8ec9adef4fe2a2aabc340f167e79
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2020-02-23 14:03:03 -0500

    groupRoute

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -15,8 +15,8 @@ class CORS
      */
     public function handle($request, Closure $next)
     {
-      return $next($request
+      return $next($request)
       ->header('Access-Control-Allow-Origin', '*')
-      ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'));
+      ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('CORS')->post('login', 'APILoginController@login');
+Route::post('login', 'APILoginController@login')->middleware('CORS')->;
 
 //Route::post('logout', 'APILoginController@logout');
 
